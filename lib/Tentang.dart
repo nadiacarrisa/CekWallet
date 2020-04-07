ec5368d1d959823752e0e9c0394eb5028f0d5652
nadiacarrisa <stephanie.nadia@ti.ukdw.ac.id>
2020-04-07 21:31:37 +0700

    Tentang

--- lib/Tentang.dart
+++ lib/Tentang.dart
@@ -4,31 +4,58 @@ class Tentang extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          backgroundColor: Color.fromRGBO(0, 149, 218, 1),
-          title: Text("Tentang"),
-        ),
-        body: new Container(
-            child: Padding(
-                padding: EdgeInsets.only(top: 30.0, right: 25.0, left: 15.0),
-                child: Column(
+      appBar: new AppBar(
+        backgroundColor: Color.fromRGBO(0, 149, 218, 1),
+        title: Text("Tentang"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          ClipPath(
+            // ini kaya bikin shape yang bisa di customize sisi nya
+            clipper: CustomShapeClipper(),
+            child: Container(
+              height: 350,
+              decoration: BoxDecoration(color: Color.fromRGBO(0, 149, 218, 1),),
+            ),
+          ),
+          Center(
+            child: Column(
+              children: <Widget>[
+                Image.asset('images/wang.png', width: 300, height: 300,),
+                Text(
+                  'Aplikasi money manager ini dibuat\nuntuk memenuhi tugas pertama dalam\nmatakuliah pemrograman hybrid\n\n'
+                      '\nyang dibuat oleh:\n'
+                      '\nWilhelmus Krisvan\t71170144'
+                      '\nStephanie Nadia\t71170145'
+                      '\nHandi Hermawan\t71170146\n',
+                  style: TextStyle(fontSize: 16, ),
+                  textAlign: TextAlign.center,
+                ),
 
-                  children: <Widget>[
-                    Text(
-                      'Aplikasi money manager'
-                          '\naplikasi ini dibuat untuk memenuhi tugas pertama dalama matakuliah'
-                          '\npemrograman hybrid\n\n'
-                          '\nyang dibuat oleh:\n'
-                          '\nWilhelmus Krisvan\t71170144'
-                          '\nStephanie Nadia\t71170145'
-                          '\nHandi Hermawan\t71170146\n',
-                      style: TextStyle(fontSize: 16, ),
-                      textAlign: TextAlign.center,
-                    )
-                  ],
-                )
-            )
-        )
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            margin: EdgeInsets.only(bottom: 20),
+            child:Text('Copyright \u00a9 2020 by Wang\'s Team',),
+          )
+        ],
+      )
     );
   }
+}
+class CustomShapeClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, 390.0 - 200);
+    path.quadraticBezierTo(size.width / 2, 280, size.width, 390.0 - 200.0);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
